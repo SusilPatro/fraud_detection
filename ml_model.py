13fdadff00635efb6f3b3607c69c0b0be26b15a4
SusilPatro <susil.patro@gmail.com>
2023-01-14 21:18:06 +0530

    added 3 code for models and will test the performance now

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print ("creating new model file for ML code")
+
+def model1():
+    print ("code for model 1")
+
+def model2():
+    print ("code for model 2")
+
+def model3():
+    print ("code for model 3")
+
